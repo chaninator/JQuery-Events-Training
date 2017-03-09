cc74d181eea5b6a4fed1261a85421ff1f213362d
Andrew Chan <accsuperstar@gmail.com>
2017-03-09 08:33:06 -0600

    complete change exercise

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -1,7 +1,27 @@
-console.log("Sanity Check: JS is working!");
+$(function() {
 
-$(document).ready(function(){
+  console.log("Sanity Check: JS is working!");
 
-  $("#time").text( Date.now() );
+  $("#time").text(Date.now());
 
-})
+  // function randomNumGenerator(1, 100) {
+  //   return Math.floor(Math.random() * (100) + 1);
+  // };
+
+  $('input').on('change', function() {
+    var leftNum = $('#left').val();
+    var rightNum = $('#right').val();
+    // $('#random').val(randomNumGenerator());
+    var sum = parseInt(leftNum) + parseInt(rightNum);
+    $('#total').val(sum);
+  });
+
+
+  // created button to clear field
+  $('#btn-clear').on('click', function() {
+    $('#left').val('');
+    $('#right').val('');
+    $('#total').val('');
+    console.log('cleared');
+  });
+});
--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -2,4 +2,12 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-})
+$('#imperatives').on('click', function(e) {
+  console.log("You clicked on: ", e.target);
+  var button = $(e.target).text();
+  $('#clickLog').append('<li>' + 'You clicked: ' + button + ' at ' + Date.now() + '</li>');
+
+});
+
+
+});
